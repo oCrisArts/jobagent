39e43b8da03a5c3ec12fecdb3f734dfeb326f093
JobAgent <jobagent@user.com>
2026-04-08 23:08:24 -0300

    ajustes na fonte de icones

--- app/layout.tsx
+++ app/layout.tsx
@@ -46,7 +46,7 @@ export default async function RootLayout({
         <link
           rel="stylesheet"
           href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css"
-          integrity="sha512-iecdLmaskl7CVJkEZSMUkrQ6usKu8zTifQDdAJxUqcKkxYJ30MzqjZY+25dQxY33Z0aEoKvAdiuotEmWWlsRLoQ=="
+          integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw=="
           crossOrigin="anonymous"
           referrerPolicy="no-referrer"
         />
